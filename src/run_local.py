da418a271c0fca5f7f1fa234659735a0f2886252
Rakesh <rockersinfo@gmail.com>
2024-04-06 11:10:54 +0530

    update command

--- src/run_local.py
+++ src/run_local.py
@@ -7,10 +7,11 @@ B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
 
-instruction = "Convert the following text from English to Hindi: \n\n {text}"
+#instruction = "Convert the following text from English to Hindi: \n\n {text}"
+instruction = "Give summary of : \n\n {text}"
 
 
-SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + E_SYS
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
 
 
@@ -26,4 +27,4 @@ llm = CTransformers(model='model/llama-2-7b-chat.ggmlv3.q4_0.bin',
 
 LLM_Chain=LLMChain(prompt=prompt, llm=llm)
 
-print(LLM_Chain.run("How are you?"))
+print(LLM_Chain.run("7 habbits of highly effective people"))
